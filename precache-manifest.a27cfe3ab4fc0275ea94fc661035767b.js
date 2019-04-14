e0016a3997f736f985880e9f5c8fd17d7661fdd9
Ryan M <ryanm@Ryans-MacBook-Pro-2.local>
2019-04-14 01:30:01 -0700

    Updates

---
+++ precache-manifest.a27cfe3ab4fc0275ea94fc661035767b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36dd8490504d4dc1a055249842a1f6e8",
+    "url": "/react-pages/static/media/CodeBattle-02.36dd8490.png"
+  },
+  {
+    "revision": "1e067ef579591dacb6c4",
+    "url": "/react-pages/static/js/runtime~main.2afdb07a.js"
+  },
+  {
+    "revision": "248843407937a0b9a5eb",
+    "url": "/react-pages/static/js/main.f1ff357d.chunk.js"
+  },
+  {
+    "revision": "3219d5abdb0e355725d6",
+    "url": "/react-pages/static/js/2.7ddc18a4.chunk.js"
+  },
+  {
+    "revision": "248843407937a0b9a5eb",
+    "url": "/react-pages/static/css/main.fbbcf499.chunk.css"
+  },
+  {
+    "revision": "3219d5abdb0e355725d6",
+    "url": "/react-pages/static/css/2.ecad39ff.chunk.css"
+  },
+  {
+    "revision": "6d1f4100621774f3322079d3dce3b385",
+    "url": "/react-pages/index.html"
+  }
+];
